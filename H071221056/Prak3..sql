3984eb8c87635c317b965df5781c8088bde48a5c
JonsAZ <90194393+Jons1104@users.noreply.github.com>
2023-10-05 22:22:59 +0800

    Update Prak3..sql

--- H071221056/Prak3..sql
+++ H071221056/Prak3..sql
@@ -1,53 +1,57 @@
-# Izzata Clarissa Salsabila
-# H071221065
-# Praktikum 3
-
-# NO1
+-- NO 1
 CREATE DATABASE praktikum3;
 
-CREATE TABLE mahasiswa (
-	NIM VARCHAR(10) PRIMARY KEY ,
-	Nama VARCHAR(50) NOT NULL ,
-	Kelas CHAR(1) NOT NULL ,
-	STATUS VARCHAR(50) NOT NULL ,
-	Nilai INT(11)
-)
-DESCRIBE mahasiswa;
-
-SELECT * FROM mahasiswa;
-INSERT INTO mahasiswa 
-VALUES 
-		('H071241056', 'Kotlina', 'A', 'Hadir', 100),
-		('H071241060', ' Pitonia', 'A', 'Alfa', 85),
-		('H071241063', 'Javano', 'A', 'Hadir', 50),
-		('H071241065', 'Ciplus Kuadra', 'B', 'Hadir', 65),
-		('H071241066', 'Pihap E', 'B', 'Hadir', 85),
-		('H071241079', 'Ruby', 'B', 'Alfa', 90);
-
-# No2
+CREATE TABLE mahasiswa(
+    NIM VARCHAR(10) NOT NULL PRIMARY KEY,
+    Nama VARCHAR(50) NOT NULL,
+    Kelas CHAR(1) NOT NULL,
+    Status VARCHAR(50) NOT NULL,
+    Nilai INT
+);
+DESCRIBE 
+INSERT INTO mahasiswa
+VALUE ('H071241056','Kotina','A','Hadir',100),
+	('H071241060','Pitonia', 'A','Alfa',85),
+	('H071241063','Javano','A', 'Hadir',50),
+	('H071241065','Ciplus kuadra','B','Hadir',65),
+	('H071241066','Pihap E', 'B', 'Hadir', 85),
+	('H071241067','Ruby', 'B','Alfa',90);
+	
+-- N0 2
 UPDATE mahasiswa
-SET Nilai = 0, Kelas = 'C'
+SET Nilai = 0,Kelas = 'C'
 WHERE STATUS = 'Alfa';
 
-# No3
+-- N0 3
 DELETE FROM mahasiswa
 WHERE Kelas = 'A' AND Nilai > 90;
 
-# No4
-INSERT INTO mahasiswa 
-VALUES ('H071221065', 'Izzata Clarissa Salsabila', 'B', 'Pindahan');
+-- N0 4 
+INSERT INTO mahasiswa
+	(NIM, Nama, Kelas, STATUS)
+VALUE 
+	('H071221050', 'Ayu widianti','B ','Pindahan');
 
 UPDATE mahasiswa
 SET Nilai = 50
 WHERE STATUS = 'Alfa';
 
 UPDATE mahasiswa
-SET Kelas = 'A'
+SET Kelas = 'A';
 
-# Tugas Tambahan
+-- soal tambahan
+INSERT INTO employees
+VALUES 
+	(1987, 'ayu', 'widianti', 'x892', 'ayu3@yahoo.com',6, 1102, 'Security'),
+	(1989, 'izza', 'clarisa', 'x899', 'izza3@yahoo.com',20, 1102, 'Security'),
+	(1982, 'fail', 'fudail', 'x891', 'fail3@yahoo.com',6, 1102, 'Security');
 
-SELECT * FROM payments
-ORDER BY amount
 
-DELETE FROM payments 
-WHERE amount <= 10000
+
+-- 
+SELECT * FROM mahasiswa;
+	
+
+SHOW TABLES ;
+DESC mahasiswa;
+
